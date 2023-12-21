582a6f4d9f07710790f87e90f08cd000d6597143
Tushar Nagar <32740788+FreAkeD146@users.noreply.github.com>
2023-12-21 10:52:52 +0530

    Added Steps to implement it.

--- Basic of Input-Output/Number of steps.c
+++ Basic of Input-Output/Number of steps.c
@@ -1,3 +1,19 @@
+/* Here are the steps to implement:
+
+1. Input the size of the arrays (n).
+2. Input values for the first array.
+3. Input values for the second array.
+4. Initialize variables for the minimum value, count, and loop index.
+5. Outer loop: Iterate until the end of the array is reached.
+ - Inner loop: Reduce the value in the first array until it's not greater than the current minimum. Increment the count for each reduction.
+ - Check if the current value in the first array is less than the --current minimum. Update the minimum value and reset the loop index to the beginning.
+ - Check if the current value in the first array is negative. Set count to -1 and exit the loop.
+ - If the current value is within the minimum and not negative, move to the next index.
+6. Print the final count value.
+
+This code seems to be performing some operation on two arrays and counting the number of reductions made to make the values in the first array less than or equal to the corresponding values in the second array. If there's anything specific you want to know or if you have further questions, feel free to ask!
+*/
+
 #include <stdio.h>
 
 int main() {
